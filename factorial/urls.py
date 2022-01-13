ca0feabeb925a3c373fd0e3bdfa17300312c3974
parasgarg1481 <parasgarg1481@gmail.com>
2022-01-13 13:50:42 +0530

    commit

--- factorial/urls.py
+++ factorial/urls.py
@@ -18,12 +18,8 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('playground.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
